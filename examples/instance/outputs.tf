f96b90ca7ba48dcce76fb4b9db403b96ecb2080b
hkavya26 <hkavya26@in.ibm.com>
2021-10-12 16:45:18 +0530

    Remove script based initialisation

--- examples/instance/outputs.tf
+++
@@ -1,7 +0,0 @@
-#########################################################################################
-# IBM Cloud Hyper Protect Crypto Services Provisioning, Initialization and Managing Keys
-# Copyright 2020 IBM
-#########################################################################################
-output "ibm-hpcs-instance" {
-  value = module.ibm-hpcs-instance
-}
