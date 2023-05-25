e5504fe52220ad1043294614dc7dadd55f23b5de
shikha-mah <shikha.mah@in.ibm.com>
2023-05-25 12:26:02 +0530

    feat: initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,45 @@
 /********************************************************************
 This file is used to implement the ROOT module.
 *********************************************************************/
+
+//TO-DO
+locals {
+  //to check - You can set up to 8 administrators and the number needs to be equal to or greater than the thresholds that you specify.
+  // failover_units must be less than or equal to the number of operational crypto units.
+
+}
+resource "ibm_hpcs" "hpcs_instance" {
+  count                = var.initialization_using_recovery_crypto_units ? 1 : 0
+  location             = var.region
+  name                 = var.service_name
+  plan                 = var.plan
+  units                = var.number_of_crypto_units
+  signature_threshold  = var.signature_threshold
+  revocation_threshold = var.revocation_threshold
+  admins {
+    name  = "admin1"
+    key   = "/tke-files/1.sigkey"
+    token = "<sensitive>"
+  }
+  admins {
+    name  = "admin2"
+    key   = "/tke-files/2.sigkey"
+    token = "<sensitive>"
+  }
+}
+
+
+resource "ibm_resource_instance" "hpcs_instance1" {
+  count = var.initialization_using_recovery_crypto_units ? 0 : 1
+
+  name              = var.service_name
+  service           = "hs-crypto"
+  location          = var.region
+  plan              = var.plan
+  resource_group_id = var.resource_group_id
+  tags              = (var.tags != null ? var.tags : null)
+  service_endpoints = (var.service_endpoints != null ? var.service_endpoints : null)
+  parameters = {
+    units = var.number_of_crypto_units
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,10 +2,115 @@
 # Input Variables
 ##############################################################################
 
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+### to do - move the following vars in examples ####
+# variable "ibmcloud_api_key" {
+#   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources."
+#   type        = string
+#   sensitive   = true
+# }
 
+variable "resource_group_id" {
+  type        = string
+  description = "The resource group ID where the Hyper Protect Crypto Service instance will be created"
+  default     = "default"
+}
+
+# variable "prefix" {
+#   description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowerccase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
+#   type        = string
+#   default     = "test-hpcs"
+# }
+
+###################################################
+
+variable "region" {
+  type        = string
+  description = "The region where you want to deploy your instance."
+  default     = "us-south"
+  // available regions are us-south and us-east only
+}
+
+variable "service_name" {
+  type        = string
+  description = "The name to give the Hyper Protect Crypto Service instance"
+}
+
+variable "plan" {
+  type        = string
+  description = "The name of the service plan that you choose for your Hyper Protect Crypto Service instance"
+  default     = "standard"
+  validation {
+    condition     = contains(["standard"], var.plan)
+    error_message = "Currently, only the standard plan is supported"
+  }
+}
+
+variable "number_of_crypto_units" {
+  type        = string
+  description = "The number of operational crypto units for your service instance"
+  default     = 2
+  #  validation {
+  #     condition     = contains([2, 3], var.number_of_crypto_units)
+  #     error_message = "Valid value of var.number_of_crypto_units is 2 or 3"
+  #   }
+}
+
+variable "tags" {
+  description = "List of resource tags to apply to resources created by this module."
+  type        = list(string)
+  default     = []
+}
+
+variable "initialization_using_recovery_crypto_units" {
+  type        = bool
+  description = "Set to true if initialization using recovery crypto units is required"
+  default     = false
+}
+
+variable "signature_threshold" {
+  type        = number
+  description = "The number of administrator signatures that is required to execute administrative commands"
+  default     = 1
+  validation {
+    condition     = contains([1, 2, 3, 4, 5, 6, 7, 8], var.signature_threshold)
+    error_message = "Valid values of var.signature_threshold is between 1 and 8"
+  }
+}
+
+variable "revocation_threshold" {
+  type        = number
+  description = "The number of administrator signatures that is required to remove an administrator after you leave imprint mode"
+  default     = 1
+  validation {
+    condition     = contains([1, 2, 3, 4, 5, 6, 7, 8], var.revocation_threshold)
+    error_message = "Valid values of var.revocation_threshold is between 1 and 8"
+  }
+}
+
+variable "signature_server_url" {
+  type        = string
+  description = "The URL and port number where the signing service. Required if you are using a third-party signing service to provide administrator signature keys"
+  default     = ""
+}
+
+# variable "admins" {
+#     type = list(object({
+#           key   = string
+#           name  = string
+#           token = string
+#     } ))
+#     description = "The list of administrators for the instance crypto units"
+# }
+
+variable "failover_units" {
+  type        = number
+  description = "The number of failover crypto units for your service instance"
+  default     = 0
+}
+
+variable "service_endpoints" {
+  type        = string
+  description = "The service_endpoints to access your service instance. Valid values are public-and-private and private-only"
+  default     = "public-and-private"
+}
 ##############################################################################
