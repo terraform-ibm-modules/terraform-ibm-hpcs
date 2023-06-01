09e4e06bb985344f608e6cc4780c0a2c90bf6636
shikha-mah <shikha.mah@in.ibm.com>
2023-06-01 12:41:45 +0530

    feat: add validation of all variables

--- main.tf
+++ main.tf
@@ -1,24 +1,29 @@
 /********************************************************************
-This file is used to implement the ROOT module.
+This file is used to implement the HPCS module.
 *********************************************************************/
 
-//TO-DO
 locals {
-  //to check - You can set up to 8 administrators and the number needs to be equal to or greater than the thresholds that you specify.
-  // failover_units must be less than or equal to the number of operational crypto units.
 
+    validate_region = var.initialization_using_recovery_crypto_units == true && contains(["us-south" , "us-east"], var.region) ? true : tobool("Currently us-south and us-east are the only supported regions for HPCS instance initialization using recovery crypto units.")
+    validate_num_of_administrators = (length(var.admins) >= 1 && length(var.admins) <= 8) ? true : tobool("At least one administrator is required for the instance crypto unit and you can set upto 8 adminsitrators.")
+    validate_admins_and_threshold = length(var.admins) >= var.signature_threshold ? true : tobool("The adminstrators of the instance crypto units need to be equal to or greater than the threshold value.")
+    validate_num_of_failover_units = var.number_of_failover_units <= var.number_of_crypto_units ? true : tobool("Number of failover_units must be less than or equal to the number of operational crypto units")
+    //failover units - Currently, you can enable this option only in the us-south and us-east region.
 }
+
 resource "ibm_hpcs" "hpcs_instance" {
   count                = var.initialization_using_recovery_crypto_units ? 1 : 0
   location             = var.region
+  resource_group_id   = var.resource_group_id
   name                 = var.service_name
   plan                 = var.plan
   units                = var.number_of_crypto_units
   signature_threshold  = var.signature_threshold
   revocation_threshold = var.revocation_threshold
+  failover_units = var.number_of_failover_units
 
   dynamic "admins" {
-    for_each = var.admins
+    for_each = nonsensitive(var.admins != null ? var.admins : [])
     content {
       name = admins.value["name"]
       key = admins.value["key"]
@@ -34,7 +39,7 @@ resource "ibm_resource_instance" "base_hpcs_instance" {
   service           = "hs-crypto"
   location          = var.region
   plan              = var.plan
-  resource_group_id = var.resource_group_id
+  resource_group_id    = var.resource_group_id
   tags              = (var.tags != null ? var.tags : null)
   service_endpoints = (var.service_endpoints != null ? var.service_endpoints : null)
   parameters = {
--- variables.tf
+++ variables.tf
@@ -2,40 +2,19 @@
 # Input Variables
 ##############################################################################
 
-### to do - move the following vars in examples ####
-# variable "ibmcloud_api_key" {
-#   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources."
-#   type        = string
-#   sensitive   = true
-# }
-
 variable "resource_group_id" {
   type        = string
-  description = "The resource group ID where the Hyper Protect Crypto Service instance will be created"
-  default     = "default"
+  description = "The resource group name where the Hyper Protect Crypto Service instance will be created"
 }
-
-# variable "prefix" {
-#   description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowerccase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
-#   type        = string
-#   default     = "test-hpcs"
-# }
-
-###################################################
 
 variable "region" {
   type        = string
   description = "The region where you want to deploy your instance."
-  default     = "us-south"
-  validation {
-    condition     = contains(["us-south", "us-east"], var.region)
-    error_message = "Currently, only us-south and us-east are the supported regions"
-  }
 }
 
 variable "service_name" {
   type        = string
-  description = "The name to give the Hyper Protect Crypto Service instance"
+  description = "The name to give the Hyper Protect Crypto Service instance. Max length allowed is 30 chars."
 }
 
 variable "plan" {
@@ -44,24 +23,8 @@ variable "plan" {
   default     = "standard"
   validation {
     condition     = contains(["standard"], var.plan)
-    error_message = "Currently, only the standard plan is supported"
+    error_message = "Only the standard plan is supported currently"
   }
-}
-
-variable "number_of_crypto_units" {
-  type        = string
-  description = "The number of operational crypto units for your service instance"
-  default     = 2
-  #  validation {
-  #     condition     = contains([2, 3], var.number_of_crypto_units)
-  #     error_message = "Valid value of var.number_of_crypto_units is 2 or 3"
-  #   }
-}
-
-variable "tags" {
-  description = "List of resource tags to apply to resources created by this module."
-  type        = list(string)
-  default     = []
 }
 
 variable "initialization_using_recovery_crypto_units" {
@@ -70,13 +33,29 @@ variable "initialization_using_recovery_crypto_units" {
   default     = false
 }
 
+variable "number_of_crypto_units" {
+  type        = number
+  description = "The number of operational crypto units for your service instance"
+  default     = 2
+  validation {
+    condition     = contains([2, 3], var.number_of_crypto_units)
+    error_message = "Allowed value of number_of_crypto_units is 2 or 3"
+  }
+}
+
+variable "tags" {
+  description = "List of resource tags to apply to resources created by this module."
+  type        = list(string)
+  default     = []
+}
+
 variable "signature_threshold" {
   type        = number
   description = "The number of administrator signatures that is required to execute administrative commands"
   default     = 1
   validation {
     condition     = contains([1, 2, 3, 4, 5, 6, 7, 8], var.signature_threshold)
-    error_message = "Valid values of var.signature_threshold is between 1 and 8"
+    error_message = "Allowed values of signature_threshold is between 1 and 8"
   }
 }
 
@@ -86,56 +65,44 @@ variable "revocation_threshold" {
   default     = 1
   validation {
     condition     = contains([1, 2, 3, 4, 5, 6, 7, 8], var.revocation_threshold)
-    error_message = "Valid values of var.revocation_threshold is between 1 and 8"
+    error_message = "Allowed values of revocation_threshold is between 1 and 8"
   }
 }
 
 variable "signature_server_url" {
   type        = string
-  description = "The URL and port number where the signing service. Required if you are using a third-party signing service to provide administrator signature keys"
+  description = "The URL and port number of the signing service. Required if you are using a third-party signing service to provide administrator signature keys"
   default     = ""
 }
-
-# variable "admins" {
-#     type = list(object({
-#           key   = string
-#           name  = string
-#           token = string
-#     } ))
-#     description = "The list of administrators for the instance crypto units"
-# }
 
 variable "admins" {
   type = list(object({
     name    = string # max length: 30 chars
-    key     = string # the absolute path and the file name of the signature key file
-    token   = string # sensitive: the administrator password to access the corresponding signature key file
+    key     = string # the absolute path and the file name of the signature key file if key files are created using TKE CLI and are not using a third-party signing service
+    # if you are using a signing service, the key name is appended to a URI that will be sent to the signing service 
+    token   = string # sensitive: the administrator password/token to authorize and access the corresponding signature key file
   }))
-  default     = [
-    {
-    name  = "admin1"
-    key   = "/tke-files/1.sigkey"
-    token = "<sensitive>"
-  },
-  {
-    name  = "admin2"
-    key   = "/tke-files/2.sigkey"
-    token = "<sensitive>"
-  }
-  ]
-#   sensitive   = true
-  description = "A list of administrators for the instance crypto units. You can set up to 8 administrators and the number needs to be equal to or greater than the thresholds that you specify."
+  sensitive   = true
+  description = "A list of administrators for the instance crypto units. You can set up to 8 administrators."
 }
 
-variable "failover_units" {
+variable "number_of_failover_units" {
   type        = number
-  description = "The number of failover crypto units for your service instance"
+  description = "The number of failover crypto units for your service instance. Default is 0 if not specified and cross-region high availability will not be enabled."
   default     = 0
+  validation {
+    condition     = contains([0, 2, 3], var.number_of_failover_units)
+    error_message = "Allowed values of failover_units is 0, 2, 3."
+  }
 }
 
 variable "service_endpoints" {
   type        = string
-  description = "The service_endpoints to access your service instance. Valid values are public-and-private and private-only"
+  description = "The service_endpoints to access your service instance. Default value is public-and-private if not specified."
   default     = "public-and-private"
+  validation {
+    condition     = contains(["public-and-private" , "private-only" ], var.service_endpoints)
+    error_message = "Allowed values of service_endpoints are public-and-private and private-only"
+  }
 }
 ##############################################################################
