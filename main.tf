e45ec7aa4d202efbff027a2ed578ce93424c433c
shikha-mah <shikha.mah@in.ibm.com>
2023-06-05 12:52:14 +0530

    fix: update variables validation

--- main.tf
+++ main.tf
@@ -4,14 +4,13 @@ This file is used to implement the HPCS module.
 
 locals {
   # tflint-ignore: terraform_unused_declarations
-  validate_region = var.initialization_using_recovery_crypto_units == true && contains(["us-south", "us-east"], var.region) ? true : tobool("Currently us-south and us-east are the only supported regions for HPCS instance initialization using recovery crypto units.")
+  validate_region = var.initialization_using_recovery_crypto_units == true ? (contains(["us-south", "us-east"], var.region) ? true : tobool("Currently us-south and us-east are the only supported regions for HPCS instance initialization using recovery crypto units.")) : true
   # tflint-ignore: terraform_unused_declarations
-  validate_num_of_administrators = (var.initialization_using_recovery_crypto_units == true && length(var.admins) >= 1 && length(var.admins) <= 8) ? true : tobool("At least one administrator is required for the instance crypto unit and you can set upto 8 adminsitrators.")
+  validate_num_of_administrators = var.initialization_using_recovery_crypto_units == true ? ((length(var.admins) >= 1 && length(var.admins) <= 8) ? true : tobool("At least one administrator is required for the instance crypto unit and you can set upto 8 adminsitrators.")) : true
   # tflint-ignore: terraform_unused_declarations
-  validate_admins_and_threshold = length(var.admins) >= var.signature_threshold ? true : tobool("The adminstrators of the instance crypto units need to be equal to or greater than the threshold value.")
+  validate_admins_and_threshold = var.initialization_using_recovery_crypto_units == true ? ((length(var.admins) >= var.signature_threshold && length(var.admins) >= var.revocation_threshold) ? true : tobool("The adminstrators of the instance crypto units need to be equal to or greater than the threshold value.")) : true
   # tflint-ignore: terraform_unused_declarations
-  validate_num_of_failover_units = var.number_of_failover_units <= var.number_of_crypto_units ? true : tobool("Number of failover_units must be less than or equal to the number of operational crypto units")
-  # failover units - Currently, you can enable this option only in the us-south and us-east region.
+  validate_num_of_failover_units = var.initialization_using_recovery_crypto_units == true ? (var.number_of_failover_units <= var.number_of_crypto_units ? true : tobool("Number of failover_units must be less than or equal to the number of operational crypto units")) : true
 }
 
 resource "ibm_hpcs" "hpcs_instance" {
